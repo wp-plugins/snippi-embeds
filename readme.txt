3d810eba13d6b8bed02d6033983efb9106d3aa5f
halgatewood <halgatewood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-12 23:40:02 +0000

    initial load
    git-svn-id: https://plugins.svn.wordpress.org/snippi-embeds/trunk@667125 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Snippi Shortcode ===
+Contributors: halgatewood
+Donate link: http://halgatewood.com/snippi-embed/
+Tags: shortcode, snippi, code, embed, pre, 
+Requires at least: 3
+Tested up to: 3.5
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed Snippi\'s from snippi.com into your blog with this handy shortcode
+
+== Description ==
+
+Easily showcase snippi code blocks on your blog.
+
+Usage:
+
+`[snippi id="c0c8lz3"]` 
+
+or 
+
+`[snippi id="c0c8lz3" type="raw"]`
+
+
+It uses the Transient API to store the snippi keeping your site nice and fast!
+
+
+== Installation ==
+
+1. Add plugin to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Include Snippi with shortcode in your pages and posts
+
+Usage:
+
+`[snippi id="c0c8lz3"]` 
+
+or 
+
+`[snippi id="c0c8lz3" type="raw"]`
+
+
+
+== Screenshots ==
+
+1. What regular snippi embed looks like
+2. With type='raw' added to the embed
+3. Embed in the Tiny MCE editor
+
+== Changelog ==
+
+= 1.0 =
+* Initial load of the plugin.
+
---
+++ snippi-embeds.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: Snippi Shortcode
+Version: 1.1
+Plugin URI: http://www.halgatewood.com/snippi-embed
+Description: Embed Snippi\'s from snippi.com into your blog with this handy shortcode
+Author: Hal Gatewood
+Author URI: http://www.halgatewood.com
+
+----
+
+Copyright 2010-2013 Hal Gatewood
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+---
+*/
+
+
+
+// THE SHORTCODE
+function snippi_shortcode($atts, $content = null)
+{
+	$id = $atts['id'];
+
+	if(!isset($id) OR $id == "") { return $content; } // NO ID
+	$type = (isset($atts['type']) == "raw") ? "raw" : "pretty";
+
+	// QUEUE CSS AND JS
+	wp_enqueue_style( 'snippi_css', plugins_url() . '/snippi-embeds/snippi.css' );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'prettify', plugins_url() . '/snippi-embeds/js/prettify.js' );
+	wp_enqueue_script( 'prettyPrint', plugins_url() . '/snippi-embeds/js/prettyPrint.js' );
+	
+	// GET TRANSIENT
+	if(get_transient('snippi_' . $type . '_' . $id))
+	{
+		$code = get_transient('snippi_' . $type . '_' . $id);
+	}
+	else
+	{
+		// GET CODE FROM SNIPPI.COM
+		if($type == "raw")
+		{
+			$code = file_get_contents("http://snippi.com/raw/{$id}");	
+		}
+		else
+		{
+			include_once('simple_html_dom.php');
+			$html = file_get_html("http://snippi.com/s/{$id}"); 
+			$code = $html->find('.code-wrapper', 0)->innertext;
+			$code = '<div class="snippi-item"><div class="code-wrapper">' . $code . '</div></div>';
+		}
+		
+		//SET TRANSIENT
+		set_transient('snippi_' . $type . '_' . $id, $code, 2629743);
+	}
+
+	$embed = '<div id="snippi-' . $id . '" class="snippi snippi ' . $type . '">';
+	do_action( 'snippi_before_html', $id );
+	$embed .= apply_filters( 'snippi_html', $code );
+	do_action( 'snippi_after_html', $id );
+	$embed .= '</div>';
+
+	return $embed;
+}
+
+add_shortcode('snippi', 'snippi_shortcode'); 
+
+?>
